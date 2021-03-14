779c1b7a21231aec66a05a7145234131387e5499
Paula <paula_mercedes_apps1oa1920@cev.com>
2021-03-14 20:46:24 +0100

    - Commit inicial

---
+++ App Opiniones/App Opiniones/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App Opiniones
+//
+//  Created by Paula on 14/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
